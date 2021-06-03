3fb77635606cf080d33e68b9637e92fd669d31fd
tenplus1 <kinsellaja@yahoo.com>
2021-06-03 08:46:14 +0100

    Initial 1.2 upload

---
+++ beds.lua
@@ -0,0 +1,54 @@
+-- support for MT game translation.
+local S = beds.get_translator
+
+-- Fancy shaped bed
+
+beds.register_bed("beds:fancy_bed", {
+	description = S("Fancy Bed"),
+	inventory_image = "beds_bed_fancy.png",
+	wield_image = "beds_bed_fancy.png",
+	tiles = {"beds_fancy_bed.png", "default_wood.png"},
+	mesh = "beds_fancy_bed.obj",
+	selectionbox = {-0.5, -0.5, -0.5, 0.5, 0.1875, 1.5},
+	collisionbox = {-0.5, -0.5, -0.5, 0.5, 0, 1.5},
+	recipe = {
+		{"", "", "group:stick"},
+		{"wool:white", "wool:white", "wool:white"},
+		{"group:wood", "group:wood", "group:wood"}
+	}
+})
+
+-- Simple shaped bed
+
+beds.register_bed("beds:bed", {
+	description = S("Simple Bed"),
+	inventory_image = "beds_bed.png",
+	wield_image = "beds_bed.png",
+	tiles = {"beds_simple_bed.png"},
+	mesh = "beds_simple_bed.obj",
+	selectionbox = {-0.5, -0.5, -0.5, 0.5, 0.06, 1.5},
+	collisionbox = {-0.5, -0.5, -0.5, 0.5, 0.06, 1.5},
+	recipe = {
+		{"wool:red", "wool:red", "wool:white"},
+		{"group:wood", "group:wood", "group:wood"}
+	}
+})
+
+-- Aliases for PilzAdam's beds mod
+
+minetest.register_alias("beds:bed_bottom_red", "beds:bed_bottom")
+minetest.register_alias("beds:bed_top_red", "beds:bed_top")
+
+-- Fuel
+
+minetest.register_craft({
+	type = "fuel",
+	recipe = "beds:fancy_bed",
+	burntime = 13
+})
+
+minetest.register_craft({
+	type = "fuel",
+	recipe = "beds:bed",
+	burntime = 12
+})
