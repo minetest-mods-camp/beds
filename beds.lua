f0a731c34c9d4716eaddc1e1016177b95613ecf0
tenplus1 <kinsellaja@yahoo.com>
2021-06-03 09:59:00 +0100

    add pink fancy and blue simple beds

--- beds.lua
+++ beds.lua
@@ -18,6 +18,21 @@ beds.register_bed("beds:fancy_bed", {
 	}
 })
 
+beds.register_bed("beds:fancy_bed_pink", {
+	description = S("Pink Fancy Bed"),
+	inventory_image = "beds_bed_fancy_pink.png",
+	wield_image = "beds_bed_fancy_pink.png",
+	tiles = {"beds_fancy_bed_pink.png", "default_wood.png"},
+	mesh = "beds_fancy_bed.obj",
+	selectionbox = {-0.5, -0.5, -0.5, 0.5, 0.1875, 1.5},
+	collisionbox = {-0.5, -0.5, -0.5, 0.5, 0, 1.5},
+	recipe = {
+		{"", "", "group:stick"},
+		{"wool:pink", "wool:pink", "wool:white"},
+		{"group:wood", "group:wood", "group:wood"}
+	}
+})
+
 -- Simple shaped bed
 
 beds.register_bed("beds:bed", {
@@ -29,7 +44,21 @@ beds.register_bed("beds:bed", {
 	selectionbox = {-0.5, -0.5, -0.5, 0.5, 0.06, 1.5},
 	collisionbox = {-0.5, -0.5, -0.5, 0.5, 0.06, 1.5},
 	recipe = {
-		{"wool:red", "wool:red", "wool:white"},
+		{"wool:white", "wool:white", "wool:white"},
+		{"group:wood", "group:wood", "group:wood"}
+	}
+})
+
+beds.register_bed("beds:bed_blue", {
+	description = S("Blue Simple Bed"),
+	inventory_image = "beds_bed_blue.png",
+	wield_image = "beds_bed_blue.png",
+	tiles = {"beds_simple_bed_blue.png"},
+	mesh = "beds_simple_bed.obj",
+	selectionbox = {-0.5, -0.5, -0.5, 0.5, 0.06, 1.5},
+	collisionbox = {-0.5, -0.5, -0.5, 0.5, 0.06, 1.5},
+	recipe = {
+		{"wool:blue", "wool:blue", "wool:white"},
 		{"group:wood", "group:wood", "group:wood"}
 	}
 })
